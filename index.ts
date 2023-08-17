6ac2154c4abc74c53788b21e93cb75e523a0ee6f
Anunay Maheshwari <anunay@dyte.io>
2023-08-18 02:52:12 +0530

    something

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 require('dotenv').config();
 import {App} from "@slack/bolt";
-// import NerdGraph from "newrelic-nerdgraph-client"
 
 const SLACK_APP_TOKEN = process.env.SLACK_APP_TOKEN!;
 const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN!;
@@ -20,23 +19,45 @@ const app = new App({
 const NerdGraph = require('newrelic-nerdgraph-client');
 const nrApp = new NerdGraph(NR_API_KEY)
 
-app.command("/findit", async ({client, respond, ack}) => {
-    await respond({
-        blocks: [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": `NO!!!!`
-                }
-            },
-        ],
-        response_type: "in_channel", // change to "in_channel" to make it visible to others
-    });
-
-    console.log("Findit: sent response")
+app.command("/findit", async ({client, respond, ack, payload}) => {
     await ack()
     console.log("Findit: sent ack")
+    // const res = await respond({
+    //     blocks: [
+    //         {
+    //             "type": "section",
+    //             "text": {
+    //                 "type": "mrkdwn",
+    //                 "text": `NO!!!!`
+    //             }
+    //         },
+    //     ],
+    //     response_type: "in_channel", // change to "in_channel" to make it visible to others
+    // });
+    const res = await client.chat.postMessage(
+        {
+          channel: process.env.GENERAL_CHANNEL!,
+          text: "Hii"
+        }
+    )
+
+    await client.chat.postMessage(
+        {
+          channel: res.channel!,
+          thread_ts: res.ts,
+          text: "Hi there!"
+        }
+    )
+
+    //console.log(payload)
+
+    // await client.files.uploadV2({
+    //   filename: `peer_reports_${PEER_ID}.json`,
+    //   content: JSON.stringify({data: 1}),
+    //   channel_id: payload.channel_id.toString()
+    // });
+
+    console.log("Findit: sent response")
 });
 
 app.command("/callstats", async ({client, respond, ack, context}) => {
@@ -47,11 +68,12 @@ app.command("/callstats", async ({client, respond, ack, context}) => {
         {headers: {"Authorization": `Bearer ${CALLSTATS_TOKEN}`}}
     )
     console.log("Callstats: data received")
-    await client.files.uploadV2({
+    const res = await client.files.uploadV2({
         filename: `peer_reports_${PEER_ID}.json`,
         content: JSON.stringify(data.data.peerReport),
         channel_id: process.env.GENERAL_CHANNEL,
     });
+    console.log(res)
     console.log("Callstats: file uploaded")
 });
 
@@ -75,41 +97,28 @@ app.command("/newrelic", async ({client, respond, ack, context}) => {
     await ack()
 });
 
-// async function nr_logs() {
-//     const { data } = await axios.post(
-//         "https://api.newrelic.com/graphql",
-//         {
-//             headers: {"API-Key": NR_API_KEY},
-//             data: {
-//             "query": "{\n   actor {\n      account(id: 3360475) {\n         nrql(query: \"SELECT * FROM Log where meetingMetadata.organizationId='7dddae64-5a93-4650-9eb0-1657d23d0bee' and metadata.error.kind='screenshare' and meetingMetadata.deviceInfo.osName='macOS' AND meetingMetadata.visitedUrl not like '%one_to_one%' SINCE 1 day AGO\") {\n            results\n         }\n      }\n   }\n}",
-//             "variables": ""
-//             }}
-//             // data: {
-//             //     "query": "{\n   actor {\n      account(id: 3360475) {\n         nrql(query: \"SELECT * FROM Log where meetingMetadata.organizationId='7dddae64-5a93-4650-9eb0-1657d23d0bee' and metadata.error.kind='screenshare' and meetingMetadata.deviceInfo.osName='macOS' AND meetingMetadata.visitedUrl not like '%one_to_one%' SINCE 1 day AGO\") {\n            results\n         }\n      }\n   }\n}",
-//             //     "variables": ""
-//             // }}
-//     )
-//     console.log(data)
+// function nr_test() {
+//     const options = {
+//         account: 3360475,
+//         query: "SELECT meetingMetadata.roomName FROM Log WHERE service.name='web-core' SINCE 1 hour ago LIMIT 5"
+//     };
+//
+//     nrApp.query(options)
+//         .then((data: any) => {
+//             console.log(data);
+//         })
+//         .catch((error: any) => {
+//             console.error(error);
+//         });
 // }
 //
-// nr_logs()
+// nr_test()
 
-function nr_test() {
-    const options = {
-        account: 3360475,
-        query: "SELECT meetingMetadata.roomName FROM Log WHERE service.name='web-core' SINCE 1 hour ago LIMIT 5"
-    };
-
-    nrApp.query(options)
-        .then((data: any) => {
-            console.log(data);
-        })
-        .catch((error: any) => {
-            console.error(error);
-        });
-}
-
-nr_test()
+// async function test() {
+//   console.log(await app.client.conversations.list())
+// }
+//
+// test()
 
 app.start().catch((error) => {
     console.error(error);
