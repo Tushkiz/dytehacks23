b7ee652c03379adaba6079b3fcddcd3434b3ac88
rohitkhirid <rohitkhirid@dyte.io>
2023-08-18 13:43:49 +0530

    chore: cleaned up code

--- index.ts
+++ index.ts
@@ -29,34 +29,10 @@ interface Org {
     name: String
 }
 
-getOrgs()
+// getOrgs()
+getOrgStats("60709d2a-c83e-477a-8199-f00ff680c44d", "607db168ba0eb5504975")
 
 
-// axios.post({
-//     url: "https://api.dyte.io/graphql",
-//     headers: {
-//       accept: "*/*",
-//       "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-//       authorization:
-//         `Bearer ${DEV_PORTAL}`,
-//       "cache-control": "no-cache",
-//       "content-type": "application/json",
-//       pragma: "no-cache",
-//       "sec-ch-ua":
-//         '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-//       "sec-ch-ua-mobile": "?0",
-//       "sec-ch-ua-platform": '"macOS"',
-//       "sec-fetch-dest": "empty",
-//       "sec-fetch-mode": "cors",
-//       "sec-fetch-site": "same-site",
-//     },
-//     referrer: "https://dev.dyte.io/",
-//     referrerPolicy: "strict-origin-when-cross-origin",
-//     body: '{"query":"\\n    query {\\n        loggedUser {\\n            id\\n            name\\n            ownedOrgs {\\n                id\\n                name\\n                accessToken\\n            }\\n        }\\n    }\\n"}',
-//     method: "POST",
-//     mode: "cors",
-//     credentials: "include",
-//   }).then(() => {});
 function getOrgs() {
     const options = {
         method: 'POST',
@@ -82,13 +58,47 @@ function getOrgs() {
     
     axios
             .request(options)
-            .then(function ({ data } : {data:Response}) {
+            .then(function ({ data }: {data: Response}) {
                 console.log(data);
                 console.log(data.data.loggedUser.name)
                 for (let org of data.data.loggedUser.ownedOrgs) {
                     console.log(org);
                 }
                 console.log(data.data.loggedUser.ownedOrgs.length)
+            })
+            .catch(function (error: any) {
+                console.error(error);
+            });
+}
+
+function getOrgStats(orgId: string, apikey: string) {
+    let date = new Date();
+    let firstDay = Date.now() - 30*24*60*60*1000;
+    let lastDay = Date.now();
+    const options = {
+        method: 'GET',
+        url: `https://api.dyte.io/v1/organizations/${orgId}/stats?startTime=${firstDay}&endTime=${lastDay}`,
+        headers: {
+            "accept": "*/*",
+            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+            "authorization": `APIKEY ${apikey}`,
+          "cache-control": "no-cache",
+          "content-type": "application/json",
+          "pragma": "no-cache",
+          "sec-ch-ua":'"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+        mode: "cors",
+        credentials: "include"
+    };
+    axios
+            .request(options)
+            .then(function ({ data }) {
+                console.log(data);
             })
             .catch(function (error: any) {
                 console.error(error);
